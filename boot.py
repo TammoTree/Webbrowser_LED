a0a0450d0dfdfd23f846991a1c9574c2ac1818b3
TammoTree <91952536+TammoTree@users.noreply.github.com>
2022-01-14 13:02:25 +0100

    Version 1

---
+++ boot.py
@@ -0,0 +1,27 @@
+# Complete project details at https://RandomNerdTutorials.com
+
+try:
+  import usocket as socket
+except:
+  import socket
+
+from machine import Pin
+import network
+
+import esp
+esp.osdebug(None)
+
+import gc
+gc.collect()
+
+ssid = 'esp32_baum'
+password = 'Baum12345'
+
+ap = network.WLAN(network.AP_IF)
+ap.config(essid=ssid, authmode=network.AUTH_WPA_WPA2_PSK, password = password)
+ap.active(True)
+print(ap.ifconfig())
+
+
+led = Pin(15, Pin.OUT)
+
